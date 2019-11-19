790f66d43a326ea6b25b5f1fc68785860784e44a
2160844 <2160844@my.ipleiria.pt>
2019-11-19 15:22:23 +0000

    login layout

--- app/Http/Controllers/LoginControllerAPI.php
+++ app/Http/Controllers/LoginControllerAPI.php
@@ -4,33 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-define('YOUR_SERVER_URL', 'http://projetodad.test');
+define('YOUR_SERVER_URL', 'http://dad.test/');
 // Check "oauth_clients" table for next 2 values:
-define('CLIENT_ID', '2');
-define('CLIENT_SECRET','kd2wsZkTDlLOfUzjR02gC01UwU7HYfeonrEWgGAr');
+define('CLIENT_ID', '6');
+define('CLIENT_SECRET','P4XPY6vnOQN1sZLM3ZOSBGYlVK1segSQ9RJqgNoh');
 
 class LoginControllerAPI extends Controller
 {
     public function login(Request $request)
     {
         $http = new \GuzzleHttp\Client;
-        $response = $http->post(YOUR_SERVER_URL.'/oauth/token', [
-        'form_params' => [
-        'grant_type' => 'password',
-        'client_id' => CLIENT_ID,
-        'client_secret' => CLIENT_SECRET,
-        'username' => $request->email,
-        'password' => $request->password,
-        'scope' => ''
-        ],
-        'exceptions' => false,
-        ]);
+            $response = $http->post(YOUR_SERVER_URL.'/oauth/token', [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => CLIENT_ID,
+                    'client_secret' => CLIENT_SECRET,
+                    'username' => $request->email,
+                    'password' => $request->password,
+                    'scope' => ''
+                ],
+                'exceptions' => false,
+            ]);
         $errorCode= $response->getStatusCode();
         if ($errorCode=='200') {
-        return json_decode((string) $response->getBody(), true);
+            return json_decode((string) $response->getBody(), true);
         } else {
-        return response()->json(
-        ['msg'=>'User credentials are invalid'], $errorCode);
+            return response()->json(
+                ['msg'=>'User credentials are invalid'], $errorCode);
         }
     }
 
---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="content">
+    <div class="title m-b-md">
+        Laravel
+    </div>
+</div>
+
+
+</template>
+<script>
+    export default {
+        data: function () {
+            return {
+                
+            }
+        }
+    }
+</script>
