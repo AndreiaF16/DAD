62ee11dc4173833c760c9d1e5263562a1f3f43c8
David Simões <2170978@my.ipleiria.pt>
2019-11-26 16:53:38 +0000

    Commit to pull

---
+++ app/Http/Controllers/OperatorControllerAPI.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\User as UserResource;
+
+class OperatorControllerAPI extends Controller
+{
+    public function Create(Request $request){
+
+    }
+}
