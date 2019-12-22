5170286feae2f7094e425e6f35f236c64ef291ea
David Simões <2170978@my.ipleiria.pt>
2019-12-22 18:42:39 +0000

    operator create income upadated

--- app/Http/Controllers/OperatorControllerAPI.php
+++ app/Http/Controllers/OperatorControllerAPI.php
@@ -10,24 +10,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\Movement as MovementResource;
+use App\Http\Controllers\Requests\IncomeOperatorRequest;
 
 class OperatorControllerAPI extends Controller
 {
-    public function registerIncome(Request $request){
-        if($request->type_payment == 'bt'){
-            $validated = $request->validate([
-                'value' => 'required|numeric|min:0.01|max:5000',
-                'email' => 'required|string|email|max:255|exists:wallets,email',
-                'type_payment' => 'required|in:c,mb,bt',
-                'iban' => 'nullable|regex:/^[A-Za-z]{2}[0-9]{23}/',
-                'source_description' => 'max:255'
-            ]);
+    public function registerIncome(IncomeOperatorRequest $request){
+        $movement = new Movement();
+
+        if($request->type_payment != 'bt'){
+            $movement->fill($request->except(['iban','source_description']));
         }else{
-            $validated = $request->validate([
-            'email' => 'required|string|email|max:255',
-            'value' => 'required|numeric|min:0.01|max:5000',
-            'type_payment' => 'required|in:c,mb,bt',
-            ]);
+            $movement->fill($request->all());
         }
 
         //Alterar balance da wallet destino
@@ -41,18 +34,14 @@ class OperatorControllerAPI extends Controller
          //Date/Time atual
         $date = Carbon::now();
 
-        $movement = new Movement();
-        $movement->fill($validated);
+        
         $movement->wallet_id = $wallet->id;
         $movement->type = "i";
         $movement->start_balance = $wallet->balance;
         $movement->end_balance = $wallet->balance + $request->value;
         $movement->transfer=0;
         $movement->date = $date->toDateTimeString();
-        if($movement->type_payment == "bt"){
-            $movement->iban = $request->iban;
-            $movement->source_description = $request->source_description;
-        }
+
         $movement->save();
 
        // return response()->json(new MovementResource($movement), 201);
